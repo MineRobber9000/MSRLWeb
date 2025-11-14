7163f42a3e8ceab52f669d3dcba6f6b8d9698491
JoeStrout <joe@strout.net>
2025-11-14 09:29:19 -0700

    Update Asteroids demo, and github pages demo.

--- update_demo.sh
+++ update_demo.sh
@@ -3,11 +3,13 @@
 # Update the demo hosted on GitHub Pages (in the docs directory),
 # from the current state of the build and assets folders.
 
+
 echo "Copying build/msrlweb.*"
 cp build/msrlweb.* docs/
 
 echo "Copying build/assets"
-cp -r build/assets docs/
+rm -rf docs/assets
+cp -r assets docs/
 
 git add docs/
 echo "docs folder updated!"
